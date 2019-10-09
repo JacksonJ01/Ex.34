13bcfb6d8eccac67a1e86b45c96a64306e8dd23c
JacksonJ01 <jacksonj@hartwick.edu>
2019-10-09 18:04:55 -0400

    i feel like i did more work than i needed to on this but it got done nonetheless.
    _Jared Jackson

--- Ex.34.py
+++ Ex.34.py
@@ -1,1 +1,47 @@
+# Jared Jackson
+# 10/09/19
+# This exercise is to teach us to access elements of list
+# ordinal == ordered, 1st
+# cardinal == cards at random, 0
 
+animals = ['bear', 'python3.6', 'peacock', 'kangaroo', 'whale', 'platypus']
+
+print("The animal at 1:")
+print(animals[1])
+print("The \"animal\" at 1 is python3.6 because it has a value of 1.")
+print("Python3.6 has a value of 1 and is at the second position.\n")
+
+print("The third (3rd) animal:")
+print(animals[2])
+print("The third (3rd) animal is at 2 and is a peacock.")
+print("The animal at 2 is the 3rd animal and is a peacock.\n")
+
+print("The first (1st) animal:")
+print(animals[0])
+print("The first (1st) animal is at 0 and is a bear.")
+print("The animal at 0 is the 1st animal and is a bear.\n")
+
+print("The animal at 3:")
+print(animals[3])
+print("The animal at 3 is a kangaroo because it has a value of 3.")
+print("Kangaroo has a value of 3 and is at the fourth position.\n")
+
+print("The fifth (5th) animal:")
+print(animals[4])
+print("The fifth (5th) animal is at 4 and is a whale.")
+print("The animal at 4 is the fifth animal and is a whale.\n")
+
+print("The animal at 2.")
+print(animals[2])
+print("The animal at 2 is a peacock because it has a value of 2.")
+print("Peacock has a value of 2 and is at the third position.\n")
+
+print("The sixth (6th) animal.")
+print(animals[5])
+print("The sixth (6th) animal is at 5 and is a platypus.")
+print("The animal at 5 is the sixth animal and is a platypus.\n")
+
+print("The animal at 4.")
+print(animals[4])
+print("The animal at 4 is a whale because it has a value of 4.")
+print("Whale has a value of 4 and is at the fifth position.\n")
